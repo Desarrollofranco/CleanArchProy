af7694966069590e4e50f7558a9fa8128cbd26e0
Franco <arnaudinfranco@gmail.com>
2020-09-20 12:26:44 -0300

    Add Course Domain Model

---
+++ CleanArch/CleanArch.Domain/Models/Curso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string UrlImg { get; set; }
+    }
+}
